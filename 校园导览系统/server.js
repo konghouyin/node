800cfc63d9a0b4b595d500fda063e261031f0033
konghouyin <1360234119@qq.com>
2018-12-31 00:57:27 +0800

    校园导览系统
    新增：
    1.注册后台
    2.实现两个页面之间的跳转
    3.数据加密传输
    修复：
    1.验证码布局异常
    优化：
    1.登录页面的效果
    2.身份验证最早开始

---
+++ 校园导览系统/server.js
@@ -0,0 +1,262 @@
+var mysql = require('mysql');
+var querystring = require('querystring');
+var cookiep = require('cookie-parser');
+var cookies = require('cookie-session');
+var express = require('express');
+var svgCaptcha = require('svg-captcha');
+var crypto = require('crypto');
+
+
+var server = express();
+ var pool = mysql.createPool({host:'localhost',user:'root',port:3306,password:'3832414122',database:'login'});
+
+server.use(cookiep('sign'));//为cookie添加签名，防篡改
+var arr = [];
+for(var i=0;i<10000;i++){
+	arr.push(Math.random()*9999999+"asdasd");
+}
+server.use(cookies({
+	name:'sess',
+	keys:arr,
+	maxAge:1000*60*5,
+}))
+
+
+server.all('*', function(req, res, next) {
+	// console.log(req.headers);
+    res.header("Access-Control-Allow-Origin", 'http://127.0.0.1:8851'); //需要显示设置来源
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials",true); //带cookies7     res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+server.use('/p.html',function(req,res){
+	console.log('asd');
+	res.writeHead(302, {'Location': 'http://127.0.0.1:8848/try/www/index.html'});
+	res.end();
+})
+
+server.use('/login',function(req,res){
+	var obj = {};
+	var message = '';
+	req.on('data',function(data){
+		message+=data;
+	})
+	req.on('end',function(){
+		obj = querystring.parse(message);
+		passRepeate(obj,res)
+	})
+})
+//登录
+
+
+server.use('/maile',function(req,res){
+	var obj = {};
+	var message = '';
+	req.on('data',function(data){
+		message+=data;
+	})
+	req.on('end',function(){
+		obj = querystring.parse(message);
+		console.log(obj);
+		console.log(req.session);
+		if(req.session.tpyzm == undefined){
+			res.write(JSON.stringify({msg:"图片验证码已过期，请重新验证！",style:1}));
+			res.end();
+		}else	if(obj.tpyzm.toLocaleLowerCase() != req.session.tpyzm){
+			res.write(JSON.stringify({msg:"图片验证码错误，请再次验证！",style:0}));
+			res.end();
+		}else{
+			req.session['yzm'] = ""+parseInt(Math.random()*9.9999)+parseInt(Math.random()*9.9999)+parseInt(Math.random()*9.9999)+parseInt(Math.random()*9.9999)+parseInt(Math.random()*9.9999)+parseInt(Math.random()*9.9999);
+			res.write(JSON.stringify({msg:"邮件已发送！",style:1}));
+			mailepass(req.session.add,req.session.yzm);//发送邮件
+			res.end();
+		}
+	})
+	
+})
+//邮箱验证
+
+server.use('/tpyzm',function(req,res){
+	var obj = {};
+	var message = '';
+	req.on('data',function(data){
+		message+=data;
+	})
+	req.on('end',function(){
+		obj = querystring.parse(message);
+		console.log(obj);
+		maileRepeate(obj.address,req,res);
+	})
+})
+//图片验证码
+
+server.use('/reg',function(req,res){
+	if(req.session.yzm){
+		var message = "";
+		req.on('data',function(data){
+			message+=data;
+		})
+		req.on('end',function(){
+			obj = querystring.parse(message);
+ 			if(obj.ans != req.session.yzm){
+ 				res.write(JSON.stringify({msg:"邮箱验证码错误，请再次验证！"}));
+ 				res.end();
+ 			}else{
+				obj.maile = req.session.add;
+				findRepeate(obj,req,res);
+			 }
+		})
+ 	}else{
+ 		res.write(JSON.stringify({msg:"未验证邮箱或邮箱验证码过期！请验证邮箱"}));
+ 		res.end();
+ 	}
+	
+	
+	console.log(req.session);
+})
+//注册
+
+server.listen(8081);
+
+
+const nodemailer = require('nodemailer');
+function mailepass(add,num){
+	console.log(add);
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.qq.com',
+		port: 465,
+		secure: true, // true for 465, false for other ports
+		auth: {
+			user: "1360234119@qq.com", // generated ethereal user
+			pass: 'hgxjirbvirzpbadb' // generated ethereal password
+		}
+	});
+	
+	let mailOptions = {
+		from: {
+			name: 'XiYouMobile',
+			address: '1360234119@qq.com'
+		}, 
+		to: add, // list of receivers
+		subject: '西邮校园导览系统--验证码', // Subject line
+		text: '[西安邮电大学]欢迎注册西邮校园导览系统，请在页面输入验证码，验证码有效期5分钟，验证码：'+num, // plain text body
+		html: '', // html body
+		}
+	
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			return console.log(error);
+		}
+		console.log("邮件发送成功----->\n"+info);
+	});
+}
+
+function findRepeate(obj,req,res){
+	console.log(obj);
+	pool.getConnection(function(err, connection){
+	  connection.query( "SELECT `name` FROM `user_message`",  function(err, data){
+	      if(err)    {
+	          throw err;
+	      }else{
+	          for(each in data){
+				  if(data[each].name == obj.name){
+					  connection.release();
+					  res.write(JSON.stringify({msg:"用户名重复！"}));
+					  res.end();
+					  return;
+				  }
+			  }
+	      }
+		  console.log("INSERT INTO `user_message` (`ID`,`name`,`pass`,`maile`) VALUES(0,'"+obj.name+"','"+obj.pass+"','"+obj.maile+"')");
+		  connection.query("INSERT INTO `user_message` (`ID`,`name`,`pass`,`maile`) VALUES(0,'"+obj.name+"','"+obj.pass+"','"+obj.maile+"')");
+		  connection.release();
+		  req.session = null;//清除session
+		  res.write(JSON.stringify({msg:"注册成功！"}));
+		  res.end();
+	  });
+	});
+}
+
+
+
+function maileRepeate(add,req,res){
+	pool.getConnection(function(err, connection){
+		connection.query( "SELECT * FROM `user_message` WHERE maile='"+add+"'",  function(err, data){
+			if(err){
+				throw err;
+			}else{
+				if(data.length==0){
+					connection.release();
+					req.session.add = add;
+					canvas(req,res);
+				}
+				else{
+					connection.release();
+					res.write(JSON.stringify({msg:"邮箱已注册！"},style=0));
+					res.end();
+				}
+			}
+		});
+	});
+}
+
+function canvas(req,res){
+	var codeConfig = {
+        size: 4,// 验证码长度
+        ignoreChars: '0o1il', // 验证码字符中排除 0o1i
+        noise: 2, // 干扰线条的数量
+		width:100,
+		viewwidth:150,
+        height: 28,
+		viewheight:40,
+    }
+    var captcha = svgCaptcha.create(codeConfig);
+    req.session.tpyzm = captcha.text.toLowerCase(); //存session用于验证接口获取文字码
+    var codeData = {
+        img:captcha.data,
+		style:1,
+    }
+    res.send(codeData);
+	res.end();
+}
+
+function passRepeate(obj,res){
+	console.log(obj);
+	pool.getConnection(function(err, connection){
+		connection.query( "SELECT * FROM `user_message` WHERE name='"+obj.name+"'",  function(err, data){
+			if(err){
+				throw err;
+			}else{
+				if(data.length==0){
+					connection.release();
+					res.write(JSON.stringify({msg:"用户名或密码有误！",style:0}));
+					res.end();
+					return;
+				}
+				if(data[0].pass == obj.pass){
+					connection.release();
+					var cookieSend = ""+obj.name+"~"+obj.pass+"~"+new Date().getTime();//保存cookie验证，防止跨站session失效
+					
+					var str = JSON.stringify(cookieSend); //明文
+					var secret = 'niyidingjiebuchulai'; //密钥--可以随便写
+					var cipher = crypto.createCipher('aes192', secret);
+					var enc = cipher.update(str, 'utf8', 'hex'); //编码方式从utf-8转为hex;
+					enc += cipher.final('hex'); //编码方式从转为hex;
+					console.log(cookieSend+"--->"+enc)//输出加密后结果
+					
+					res.cookie('pbl',enc,{path:'/',maxAge:8*1000,signed:true});
+					res.write(JSON.stringify({msg:"登录成功！",style:1,url:"http://127.0.0.1:8851/%E6%A0%A1%E5%9B%AD%E5%AF%BC%E8%A7%88%E7%B3%BB%E7%BB%9F/www/index.html"}));
+					res.end();
+					return;
+				}
+				else{
+					connection.release();
+					res.write(JSON.stringify({msg:"用户名或密码有误！",style:0}));
+					res.end();
+				}
+			}
+		});
+	});
+}
--- 校园导览系统/www/src/index.js
+++ 校园导览系统/www/src/index.js
@@ -1,9 +1,9 @@
 var points = [{
 		id: 1,
 		positionX: 650,
-		positionY: 630,
+		positionY: 620,
 		style: "景观",
-		level: 1.35,
+		level: 1.3,
 		name: "喷泉广场",
 		img: ["http://english.xupt.edu.cn/images/13/07/10/ti1gatucb1/13fc7c83a14.jpg",
 			"http://imgsrc.baidu.com/forum/w%3D580/sign=f32f28adb812c8fcb4f3f6c5cc0292b4/5c95d739b6003af3bb2386ed342ac65c1138b66c.jpg",
@@ -34,14 +34,78 @@ var points = [{
 	},
 	{
 		id: 2,
-		positionX: 350,
-		positionY: 230,
-		style: "教育",
-		level: 1.8,
-		name: "喷泉广场",
+		positionX: 550,
+		positionY: 200,
+		style: "体育",
+		level: 1,
+		name: "大操场",
 		img: [],
-		text: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;景观位于西安邮电大学长安校区西区北门，是西邮著名地标，由象征邮政的鸽子雕像，和象征电子技术的抽象雕像组成，除此之外还配有喷泉，同学们戏称为水煮鸽子。",
-		message: []
+		text: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;大型室外活动的场所，运动会，军训等。有一个足球场和400m跑道。",
+		message: [
+			{
+					name:"长安通",
+					thing:"我要踢球",
+					time:"2018-12-29"
+				}
+		]
+	},
+		{
+		id: 3,
+		positionX: 250,
+		positionY: 600,
+		style: "教育",
+		level: 1,
+		name: "逸夫楼",
+		img: [],
+		text: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;西安邮电大学东区，教学楼，是计算机学院，数字艺术传媒学院，研究生的上课和实验场所。也是3G实验室的所在地！！！",
+		message: [{
+				name:"offer",
+				thing:"3G--nb",
+				time:"2018-12-29"
+			}
+		]
+	},
+	{
+		id: 4,
+		positionX: 620,
+		positionY: 230,
+		style: "体育",
+		level: 1.3,
+		name: "体育馆",
+		img: [],
+		text: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;西安邮电大学体育场，内设有排球场地，篮球场地，乒乓球，羽毛球。每年迎新杯比赛在这里举行",
+		message: [{
+				name:"羽协",
+				thing:"场地要是能随时使用就好",
+				time:"2018-12-30"
+			},
+			{
+					name:"健身达人",
+					thing:"能不能增加一些健身器械",
+					time:"2018-12-29"
+				}
+		]
+	},
+	{
+		id: 5,
+		positionX: 650,
+		positionY: 740,
+		style: "基础设施",
+		level: 1,
+		name: "北门",
+		img: [],
+		text: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;西邮正门，最大的门",
+		message: [{
+				name:"pp",
+				thing:"哈哈，够大。",
+				time:"2018-12-30"
+			},
+			{
+					name:"9277",
+					thing:"是个门",
+					time:"2018-12-29"
+				}
+		]
 	}
 ];
 var sides = [];
@@ -50,7 +114,7 @@ var paths = [];
 
 function showall(level) {
 	for (each in points) {
-		if (points[each].level < level) {
+		if (points[each].level <= level) {
 			try {
 				Point.prototype.clear(points[each]);
 			} catch (e) {}
@@ -78,11 +142,14 @@ var discuss = document.getElementsByClassName('discuss_thing')[0];
 
 Point.prototype = {
 	click: function(point) {
+		var oo = document.getElementsByClassName('oo')[0];
+		oo.style.maxHeight = "500px";
+		oo.scrollTo(0,0);//页面归零
 		search.value = point.name;
 		pointName.innerHTML = point.name;
 		style.innerHTML = point.style;
 		text.innerHTML = point.text;
-		
+
 		while (pic.children.length != 0) {
 			pic.children[0].remove();
 		}
@@ -90,13 +157,13 @@ Point.prototype = {
 		while (discuss.children.length != 0) {
 			discuss.children[0].remove();
 		}
-		
-		if(point.img.length==0){
+
+		if (point.img.length == 0) {
 			var a = document.createElement('img');
 			a.setAttribute('src', './src/pic/no_pic.png');
 			a.setAttribute('alt', point.name);
 			pic.appendChild(a);
-		}else{
+		} else {
 			for (each in point.img) {
 				var a = document.createElement('img');
 				a.setAttribute('src', point.img[each]);
@@ -104,15 +171,15 @@ Point.prototype = {
 				pic.appendChild(a);
 			}
 		}
-		
-		if(point.message.length==0){
+
+		if (point.message.length == 0) {
 			var a = document.createElement('div');
 			a.setAttribute('class', 'thing');
 			a.innerHTML = "暂无评论";
 			discuss.appendChild(a);
-		}else{
+		} else {
 			for (each in point.message) {
-			
+
 				var a = document.createElement('div');
 				a.setAttribute('class', 'name');
 				a.innerHTML = point.message[each].name;
@@ -130,7 +197,7 @@ Point.prototype = {
 				discuss.appendChild(d);
 			}
 		}
-		
+
 	},
 	show: function(point) {
 		var map = document.getElementsByClassName("map")[0];
@@ -158,7 +225,7 @@ var clickEvent = document.getElementsByClassName('map')[0];
 clickEvent.addEventListener('click', function(e) {
 	if (e.path.length == 7) {
 		for (each in points) {
-			if (points[each].id == e.path[0].point){
+			if (points[each].id == e.path[0].point) {
 				Point.prototype.click(points[each]);
 				break;
 			}
@@ -166,8 +233,15 @@ clickEvent.addEventListener('click', function(e) {
 
 	}
 })
-
+clickEvent.addEventListener('mousedown', function() {
+	document.getElementsByClassName('wrap')[0].style.cursor = "url('./src/pic/closedhand.cur'),auto";
+})
+clickEvent.addEventListener('mouseup', function() {
+	document.getElementsByClassName('wrap')[0].style.cursor = "url('./src/pic/openhand.cur'),auto";
+})
 
 
 
 //style:1风景 2教育 3餐饮 4住宿
+showall(1);//默认初始化显示所有信息
+
